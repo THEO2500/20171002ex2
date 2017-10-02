81bba94c17c16b0f9513977b2165489eada8b348
THEO2500 <worldpeace871219@gmail.com>
2017-10-02 15:13:43 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String a;
+	char b;
+	a=in.next();
+	b=in.next().charAt(0);
+	System.out.println(a.replace(b,'\n'));
+
+
+
+    }
+}
